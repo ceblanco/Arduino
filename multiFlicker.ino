3001c1a6afa37e1260a8a559ae0764cd77f8e3c2
Cathy E. Blanco <cblanco@acm.org>
2014-08-06 19:39:37 -0700

    Initial commit

---
+++ multiFlicker.ino
@@ -0,0 +1,35 @@
+//Multiple LED flicker by gotcha (2010)
+//
+//
+#include   <TrueRandom.h>   //Include the TrueRandom Library
+
+int ledPin1 = 8; //Set the number of LEDs you use.
+int ledPin2 = 9;
+int ledPin3 = 10;
+int ledPin4 = 11;
+int ledPin5 = 12;
+int ledPin6 = 13;
+//int ledPin4 = 6;
+//You can use up to 6 LED pins
+
+void setup()
+{
+pinMode(ledPin1, OUTPUT); //Sets the LED pins as output pins.
+pinMode(ledPin2, OUTPUT); //Use the same pins used above
+pinMode(ledPin3, OUTPUT);
+pinMode(ledPin4, OUTPUT);
+pinMode(ledPin5, OUTPUT);
+pinMode(ledPin6, OUTPUT);
+}
+
+void loop()
+{ //Sets a random value of LED voltage in the range of 0V - 5V with PWM
+//TrueRandom.random(0,255)
+analogWrite(ledPin1, 125+TrueRandom.random(0,100)); //Use the same pins used above
+analogWrite(ledPin2, 125+TrueRandom.random(0,100));
+analogWrite(ledPin3, 125+TrueRandom.random(0,100));
+analogWrite(ledPin4, 125+TrueRandom.random(0,100));
+analogWrite(ledPin5, 125+TrueRandom.random(0,100));
+analogWrite(ledPin6, 125+TrueRandom.random(0,100));
+delay(TrueRandom.random(40,70)); //Limits the speed.
+}
